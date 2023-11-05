ad1cba0fe4183b5a48ed32818c94f815ac6fc3d9
Dave Miller <dave.miller@bioss.ac.uk>
2023-11-05 21:25:17 +0000

    figure code and figure

---
+++ figures.R
@@ -0,0 +1,13 @@
+# make figures for tipsheet
+
+library(mgcv)
+set.seed(2)
+
+dat <- gamSim(1,n=400,dist="normal",scale=2)
+b <- gam(y~s(x0)+s(x1)+s(x2)+s(x3),data=dat)
+
+# gam.check plot
+pdf("gamcheck.pdf", width=7, height=7)
+gam.check(b)
+dev.off()
+
